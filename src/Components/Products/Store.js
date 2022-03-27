0f79243ca371b545aee01070f740ae8ca4fb0c26
Badhon chaki <fboy4069@gmail.com>
2022-03-27 13:13:03 +0600

    cart item displayed

--- src/Components/Products/Store.js
+++ src/Components/Products/Store.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import Name from '../../Name/Name';
 import Laptop from './Laptop/Laptop';
 import './Store.css'
 const Store = () => {
     const [laptops, setLaptops] = useState([0]);
 
-    const [cart, setCart] = useState([0]);
+    const [names, setNames] = useState([0]);
 
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setLaptops(data))
 
-    }, []);
-
+    }, [])
     const handleAddToCart = (laptop) => {
-        const newCart = [...cart, laptop]
-        setCart(newCart);
+        const newName = [...names, laptop];
+        setNames(newName);
 
     }
     return (
@@ -28,7 +28,9 @@ const Store = () => {
             </div>
             <div className='cart-container'>
                 <h2>Slect One Item</h2>
-                <h3>{cart.length}</h3>
+                <h3>
+                    {names.map(name => <Name name={name}></Name>)}
+                </h3>
                 <button className='suggest-btn'>Suggest One</button>
                 <br />
                 <button className='reset-btn'>Reset</button>
---
+++ src/Name/Name.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Name = (props) => {
+    const { name } = props.name
+    return (
+        <div>
+            {name}
+        </div>
+    );
+};
+
+export default Name;
